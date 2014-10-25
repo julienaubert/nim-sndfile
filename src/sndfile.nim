4ba0b653af2781c5b449a24db5774558c144aba2
Simon Krauter <krauter.simon@arcor.de>
2014-10-25 15:59:30 +0200

    Added more procedures; Cleaned-up code

--- src/sndfile.nim
+++ src/sndfile.nim
@@ -7,34 +7,80 @@ else:
 
 {.pragma: libsnd, cdecl, dynlib: soname.}
 
-
 type
-    TSNDFILE* = cint
+  TSNDFILE* = cint
 
-    TFILE_MODE* = enum
-        READ    = cint(0x10),
-        WRITE   = cint(0x20),
-        RDWR    = cint(0x30)
+  TFILE_MODE* = enum
+      READ    = cint(0x10),
+      WRITE   = cint(0x20),
+      RDWR    = cint(0x30)
 
-    TCOUNT* = int64
+  TCOUNT* = int64
 
-    TINFO* {.pure final.} = object
-        frames*: TCOUNT
-        samplerate*: cint
-        channels*: cint
-        format*: cint
-        sections*: cint
-        seekable*: cint
+  TINFO* {.pure final.} = object
+      frames*: TCOUNT
+      samplerate*: cint
+      channels*: cint
+      format*: cint
+      sections*: cint
+      seekable*: cint
 
-    TBOOL* = enum
-        SF_FALSE    = cint(0),
-        SF_TRUE     = cint(1)
+  TBOOL* = enum
+      SF_FALSE    = 0
+      SF_TRUE     = 1
 
-    TWHENCE* = enum
-        SEEK_SET    = cint(0), # The offset is set to the start of the audio data plus offset (multichannel) frames.
-        SEEK_CUR    = cint(1), # The offset is set to its current location plus offset (multichannel) frames.
-        SEEK_END    = cint(2)  # The offset is set to the end of the data plus offset (multichannel) frames.
-
+  TWHENCE* = enum
+      SEEK_SET    = 0 # The offset is set to the start of the audio data plus offset (multichannel) frames.
+      SEEK_CUR    = 1 # The offset is set to its current location plus offset (multichannel) frames.
+      SEEK_END    = 2  # The offset is set to the end of the data plus offset (multichannel) frames.
+  
+  TCOMMAND* = enum
+        SFC_GET_LIB_VERSION             = 0x1000
+        SFC_GET_LOG_INFO                = 0x1001
+        SFC_GET_CURRENT_SF_INFO         = 0x1002
+        SFC_GET_NORM_DOUBLE             = 0x1010
+        SFC_GET_NORM_FLOAT              = 0x1011
+        SFC_SET_NORM_DOUBLE             = 0x1012
+        SFC_SET_NORM_FLOAT              = 0x1013
+        SFC_SET_SCALE_FLOAT_INT_READ    = 0x1014
+        SFC_SET_SCALE_INT_FLOAT_WRITE   = 0x1015
+        SFC_GET_SIMPLE_FORMAT_COUNT     = 0x1020
+        SFC_GET_SIMPLE_FORMAT           = 0x1021
+        SFC_GET_FORMAT_INFO             = 0x1028
+        SFC_GET_FORMAT_MAJOR_COUNT      = 0x1030
+        SFC_GET_FORMAT_MAJOR            = 0x1031
+        SFC_GET_FORMAT_SUBTYPE_COUNT    = 0x1032
+        SFC_GET_FORMAT_SUBTYPE          = 0x1033
+        SFC_CALC_SIGNAL_MAX             = 0x1040
+        SFC_CALC_NORM_SIGNAL_MAX        = 0x1041
+        SFC_CALC_MAX_ALL_CHANNELS       = 0x1042
+        SFC_CALC_NORM_MAX_ALL_CHANNELS  = 0x1043
+        SFC_GET_SIGNAL_MAX              = 0x1044
+        SFC_GET_MAX_ALL_CHANNELS        = 0x1045
+        SFC_SET_ADD_PEAK_CHUNK          = 0x1050
+        SFC_SET_ADD_HEADER_PAD_CHUNK    = 0x1051
+        SFC_UPDATE_HEADER_NOW           = 0x1060
+        SFC_SET_UPDATE_HEADER_AUTO      = 0x1061
+        SFC_FILE_TRUNCATE               = 0x1080
+        SFC_SET_RAW_START_OFFSET        = 0x1090
+        SFC_SET_DITHER_ON_WRITE         = 0x10A0
+        SFC_SET_DITHER_ON_READ          = 0x10A1
+        SFC_GET_DITHER_INFO_COUNT       = 0x10A2
+        SFC_GET_DITHER_INFO             = 0x10A3
+        SFC_GET_EMBED_FILE_INFO         = 0x10B0
+        SFC_SET_CLIPPING                = 0x10C0
+        SFC_GET_CLIPPING                = 0x10C1
+        SFC_GET_INSTRUMENT              = 0x10D0
+        SFC_SET_INSTRUMENT              = 0x10D1
+        SFC_GET_LOOP_INFO               = 0x10E0
+        SFC_GET_BROADCAST_INFO          = 0x10F0
+        SFC_SET_BROADCAST_INFO          = 0x10F1
+        SFC_GET_CHANNEL_MAP_INFO        = 0x1100
+        SFC_SET_CHANNEL_MAP_INFO        = 0x1101
+        SFC_RAW_DATA_NEEDS_ENDSWAP      = 0x1110
+        SFC_WAVEX_SET_AMBISONIC         = 0x1200
+        SFC_WAVEX_GET_AMBISONIC         = 0x1201
+        SFC_SET_VBR_ENCODING_QUALITY    = 0x1300
 
 proc open*(path: cstring, mode: TFILE_MODE, sfinfo: ptr TINFO): ptr TSNDFILE  {.libsnd, importc: "sf_open".}
 
@@ -42,16 +88,21 @@ proc format_check*(info: ptr TINFO): TBOOL {.libsnd, importc: "sf_format_check".
 
 proc seek*(sndfile: ptr TSNDFILE, frames: TCOUNT, whence: TWHENCE): TCOUNT {.libsnd, importc: "sf_seek".}
 
-proc read_int*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cint, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_read_int".}
-
-proc readf_short*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cshort, frames: TCOUNT): TCOUNT {.libsnd, importc: "sf_readf_short".}
-
-proc read_float*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cfloat, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_read_float".}
+proc command*(sndfile: ptr TSNDFILE, cmd: TCOMMAND, data: pointer, datasize: cint): cint {.libsnd, importc: "sf_command".}
 
 proc error*(sndfile: ptr TSNDFILE): cint {.libsnd, importc: "sf_error".}
 
 proc strerror*(sndfile: ptr TSNDFILE): cstring {.libsnd, importc: "sf_strerror".}
 
+proc read_short*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cshort, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_read_short".}
+proc read_int*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cint, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_read_int".}
+proc read_float*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cfloat, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_read_float".}
+proc read_double*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cdouble, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_read_double".}
+
+proc readf_short*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cshort, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_readf_short".}
+proc readf_int*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cint, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_readf_int".}
+proc readf_float*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cfloat, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_readf_float".}
+proc readf_double*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cdouble, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_readf_double".}
 
 when isMainModule:
     var info: TINFO
