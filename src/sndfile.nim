908f2578acf0eb6f3e294d9d013305d325e7b709
James Bradbury <jamesbradbury93@gmail.com>
2020-03-30 00:07:55 +0100

    remove test

--- src/sndfile.nim
+++ src/sndfile.nim
@@ -181,24 +181,4 @@ proc readf_int*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cint, items: TCOUNT): TCO
 proc readf_float*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cfloat, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_readf_float".}
 proc readf_double*(sndfile: ptr TSNDFILE, buffer_ptr: ptr cdouble, items: TCOUNT): TCOUNT {.libsnd, importc: "sf_readf_double".}
 
-when isMainModule:
-    var info: TINFO
-    var sndfile: ptr TSNDFILE
-    info.format = 0
-
-    snd_file = open("test.wav", READ, cast[ptr TINFO](info.addr))
-    
-    echo info
-    # expect info to match snd file header
-    echo format_check(cast[ptr TINFO](info.addr))
-    
-    # expect 5
-    echo seek(snd_file, 5, SEEK_SET)
-    discard seek(snd_File, 0, SEEK_SET)
-    
-    let num_items = cast[cint](info.channels * info.frames)
-    echo num_items
-    var buffer = newSeq[cint](num_items)
-    let items_read = read_int(snd_file, buffer[0].addr, num_items)
-    echo items_read
 
